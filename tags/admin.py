9b9feb6836ae3780730f5b35a05b2030758626d5
sangeeta <sjbitcode@gmail.com>
2015-01-27 05:19:36 +0000

    registered models

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tag, TagRelation
 
 # Register your models here.
+@admin.register(Tag, TagRelation)
+class DefaultAdmin(admin.ModelAdmin):
+    pass
--- tags/views.py
+++ tags/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, Http404, HttpResponseForbidden
+from main import *
+from tags.models import Tag, TagRelation
+from django.views.generic import DetailView, ListView, TemplateView
 
 # Create your views here.
 def index(request):
   return HttpResponse("hi")
+
