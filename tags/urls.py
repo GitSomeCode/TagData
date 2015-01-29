95317e54a998d096df46eb13349a55e72b3777a1
sangeeta <sjbitcode@gmail.com>
2015-01-29 06:57:30 +0000

    links for pages

--- tags/urls.py
+++ tags/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
   url(r'^$', TemplateView.as_view(template_name="tags/index.html"), name="index"),
   url(r'^tag/$', views.DisplayRelations.as_view(), name="relations"), 
   url(r'^compare/tag/$', views.CompareLastFM.as_view(), name="compare"),
+  url(r'^about/$', TemplateView.as_view(template_name="tags/about.html"), name="about"),
+  url(r'^contact/$', TemplateView.as_view(template_name="tags/contact.html"), name="contact"),
   #url(r'^', views.tagData, name="tagData"),                     
 )
